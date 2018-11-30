3d81ad99e03f6a3256a7072d34028b21769b489c
amartinez2 <austinamartinez2@gmail.com>
2018-11-29 18:27:37 -0800

    adjusting crossorigins

--- src/main/java/ms/robot/robotms/RobotController.java
+++ src/main/java/ms/robot/robotms/RobotController.java
@@ -3,8 +3,6 @@ package ms.robot.robotms;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,18 +16,20 @@ public class RobotController {
     @Autowired
     RobotRepository robotRepository;
 
-    @CrossOrigin()
+    @CrossOrigin("http://localhost:4200")
     @GetMapping ("/allRobots")
     public List<Robot> getAll () {
         return robotRepository.findAll();
     }
 
+    @CrossOrigin("http://localhost:4200")
     @GetMapping("/name/{name}")
     public Robot getRobotByName (@PathVariable String name) {
         Robot result = robotRepository.findByName(name);
         return result;
     }
 
+    @CrossOrigin("http://localhost:4200")
     @GetMapping("/id/{id}")
     public Robot getRobotById (@PathVariable String id) {
         Robot result = robotRepository.findBy_id(id);
